2d2d5a98c0575096ce09953bf0ac2223a80d4134
DanielNigrab <bgrln347@gmail.com>
2023-03-10 19:26:24 +0300

    Initial commit

---
+++ src/main/java/com/example/socksstoragerest/SocksStorageRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.socksstoragerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksStorageRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksStorageRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socksstoragerest/SocksStorageRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socksstoragerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStorageRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
